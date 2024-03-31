43d4192fe39c56726b28cfd44bde1d62a3ac3d6f
anujrawat2005 <anujrawat724@gmail.com>
2024-03-31 10:53:34 +0530

    episode3

--- App.js
+++ App.js
@@ -21,16 +21,29 @@
  * 
  */
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const parent= React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child"},
-[React.createElement("h1",{},"I am h1 "),React.createElement("h2",{},"I am h2 tag")] ),
-React.createElement("div",{id : "child2"},[React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]
-)]);
+// React.createElement = object =render
+
+const anuj =<a>anuj rawat03022005</a>
+
+const title =  (
+    <h1 className = "head" tabIndex="5">
+       {anuj}
+       anuj rawat using jsx
+       </h1>
+)
+;
 
 
-console.log(parent); //object
+// writing java script in jsx
+const HeadingComponent = () =>  (
+    <div id = "rawat">
+     {title}
+        <h1 className = "heading" tabIndex="4">anuj rawat functional component</h1>
+ </div>
+);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root =ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent />);
